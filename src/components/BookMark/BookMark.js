50c720fa629a1f119bb8d80aad39f9bd7afb53c9
gopa11313 <gopalaaa9819525993@gmail.com>
2023-07-19 20:55:26 -0400

    chnagess

--- src/components/BookMark/BookMark.js
+++ src/components/BookMark/BookMark.js
@@ -1,15 +1,47 @@
-import { View,Text ,SafeAreaView,StatusBar,StyleSheet} from "react-native";
+import { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  TextInput,
+  Button,
+} from "react-native";
 
-export default function BookMark({navigation}){
-    return(
-        <SafeAreaView style={styles.container}>
-            <Text>Boomk Mark Page</Text>
-        </SafeAreaView>
-    )
-};
-const styles =  StyleSheet.create({
-    container:{
-        flex:1,
-        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    }
-})
+export default function BookMark({ navigation }) {
+  const [age1, setAge1] = useState("");
+  const [age2, setAge2] = useState("");
+  useEffect(() => {
+    console.log("what's the currect age:" + age1);
+  });
+  const calculate = () => {
+    const sum = parseInt(age1) + parseInt(age2);
+    console.log("This is my age:" + sum);
+  };
+  return (
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={{ borderWidth: 1 }}
+        placeholder="Enter your age"
+        keyboardType="numeric"
+        value={age1}
+        onChangeText={setAge1}
+      />
+      <TextInput
+        style={{ borderWidth: 1 }}
+        placeholder="Enter your age"
+        keyboardType="numeric"
+        value={age2}
+        onChangeText={setAge2}
+      />
+      <Button title="calculate" onPress={calculate} />
+    </SafeAreaView>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+});
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,68 @@
-import { Text,StyleSheet, View ,SafeAreaView,StatusBar } from "react-native";
+import { useState } from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  SafeAreaView,
+  StatusBar,
+  Button,
+} from "react-native";
 export default function Home({ navigation }) {
+  const increse = () => {
+    NumberFuncation(number + 1);
+    showErrorFunction(false);
+  };
+  const decrease = () => {
+    if (number == 95) {
+      showErrorFunction(true);
+      return;
+    }
+    NumberFuncation(number - 1);
+  };
+  const Reset = () => {
+    NumberFuncation(99);
+    showErrorFunction(false);
+  };
+  const [number, NumberFuncation] = useState(99);
+  const [showError, showErrorFunction] = useState(false);
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Home Page</Text>
+      <View style={styles.inner}>
+        <Text style={styles.myLabel}>Home Page</Text>
+        <Text style={{ fontSize: 20 }}>press the button to update</Text>
+        <Text
+          style={{
+            fontSize: 99,
+            color: showError ? "red" : "black",
+            textDecorationLine: showError ? "line-through" : "none",
+          }}
+          value={number}
+        >
+          {number}
+        </Text>
+
+        <Button title="Incrase" onPress={increse} />
+
+        <Button title="Decrease" onPress={decrease} />
+        <Button title="Reset" onPress={Reset} />
+        {/* coditional satatement if the showError is true it will show */}
+
+        {showError && <Text>You cannot go below than 95</Text>}
+      </View>
     </SafeAreaView>
   );
 }
-const styles =  StyleSheet.create({
-    container:{
-        flex:1,
-        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    }
-})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  inner: {
+    padding: 20,
+  },
+  myLabel: {
+    fontSize: 25,
+    color: "blue",
+    fontStyle: "bold",
+  },
+});
